5b4dad1a1a0188f8d548c201608f52eb72270d14
tobias-barakaa <ngairato@gmail.com>
2024-10-13 16:43:19 +0300

    first request

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -3,7 +3,7 @@ import { MessagesModule } from './messages/messages.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MessagesModule);
   await app.listen(3000);
 }
 bootstrap();
