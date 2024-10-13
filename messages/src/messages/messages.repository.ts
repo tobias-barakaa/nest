696ef91b6968dd0df005c093fa4dd6f5297b872a
tobias-barakaa <ngairato@gmail.com>
2024-10-13 22:45:22 +0300

    dependency injection

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -2,22 +2,22 @@ import { readFile, writeFile } from "fs/promises";
 
 export class MessagesRepository {
    async findOne(id: string) {
-    const contents = await readFile('messages.json', 'utf-8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages[id];
 
     }
     async findAll() {
-        const contents = await readFile('message.json', 'utf-8');
+        const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
         return messages;
 
     }
 
     async create(content: string) {
-        const contents = await readFile('messages.json', 'utf-8');
+        const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
-        const id = Math.floor(Math.random() * messages.length)
+        const id = Math.floor(Math.random() * 5)
         messages[id] = {id, content};
 
         await writeFile('messages.json', JSON.stringify(messages));
